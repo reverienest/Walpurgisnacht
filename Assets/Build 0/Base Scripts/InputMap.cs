e94d13c702589da39651ee1a0a287896c5de7dfc
TheGrim159 <47199533+TheGrim159@users.noreply.github.com>
2019-09-30 16:21:48 -0400

    some stuff

---
+++ Assets/Build 0/Base Scripts/InputMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Action
+{
+    //PUT THIS SHIT IN ALL CAPS
+    Up, Down, Left, Right, Fire1, Fire2, Fire3, shinMalphur, spellCard
+}
+
+public class InputMap : MonoBehaviour
+{
+    IDictionary<Action, KeyCode> act = new Dictionary<Action, KeyCode>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        act.Add(Action.Up, KeyCode.W);
+        act.Add(Action.Down, KeyCode.S);
+        act.Add(Action.Left, KeyCode.A);
+        act.Add(Action.Right, KeyCode.D);
+        act.Add(Action.Fire1, KeyCode.)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
