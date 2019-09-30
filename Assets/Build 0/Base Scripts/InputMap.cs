b09be4a65547b623577bdcb4ad70936adb168635
TheGrim159 <47199533+TheGrim159@users.noreply.github.com>
2019-09-30 17:05:48 -0400

    made it work

--- Assets/Build 0/Base Scripts/InputMap.cs
+++ Assets/Build 0/Base Scripts/InputMap.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public enum Action
 {
-    //PUT THIS SHIT IN ALL CAPS
-    Up, Down, Left, Right, Fire1, Fire2, Fire3, shinMalphur, spellCard
+    UP, DOWN, LEFT, RIGHT, PRIM, HEAVY, INTRIN, LAST_WORD, SPELL_CARD
 }
 
 public class InputMap : MonoBehaviour
@@ -14,13 +13,33 @@ public class InputMap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        act.Add(Action.Up, KeyCode.W);
-        act.Add(Action.Down, KeyCode.S);
-        act.Add(Action.Left, KeyCode.A);
-        act.Add(Action.Right, KeyCode.D);
-        act.Add(Action.Fire1, KeyCode.)
-
+        act.Add(Action.UP, KeyCode.UpArrow);
+        act.Add(Action.DOWN, KeyCode.DownArrow);
+        act.Add(Action.LEFT, KeyCode.LeftArrow);
+        act.Add(Action.RIGHT, KeyCode.RightArrow);
+        act.Add(Action.PRIM, KeyCode.Z);
+        act.Add(Action.HEAVY, KeyCode.X);
+        act.Add(Action.INTRIN, KeyCode.C);
+        act.Add(Action.LAST_WORD, KeyCode.A);
+        act.Add(Action.SPELL_CARD, KeyCode.D);
     }
+    bool Input(Action action)
+    {
+        return InputMap.GetKey(act[action]);
+    }
+    void Update(Action action, KeyCode key)
+    {
+        act.Add(action, key);
+    }
+    bool InputUp(Action action)
+    {
+        return InputMap.GetKeyUp(act[action]);
+    }
+    bool InputDown(Action action)
+    {
+        return InputMap.GetKeyDown(act[action]);
+    }
+
 
     // Update is called once per frame
     void Update()
