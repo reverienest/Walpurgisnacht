0438f9b33b7aaeb5ca66e6f07416f35066777d78
Arshad <iarsh930@gmail.com>
2019-09-29 21:27:33 -0400

    Attached UISingleton to PlayerRed directly, code style fixes

--- Assets/Game/Singletons/UISingleton.cs
+++ Assets/Game/Singletons/UISingleton.cs
@@ -7,21 +7,34 @@ public class UISingleton : Singleton<UISingleton>
 {
     protected UISingleton() { }
 
-    /*
-    * Pass UI Components into the singleton here so they can be manipulated directly.
-    */
-
-    private Image[] Player1Hearts;
-    private Sprite fullHeart;
-    private Sprite emptyHeart;
-
-    public void Setup(Image[] _Player1Hearts, Sprite _fullHeart, Sprite _emptyHeart)
+    public void Start()
     {
-        Player1Hearts = _Player1Hearts;
-        fullHeart = _fullHeart;
-        emptyHeart = _emptyHeart;
+        Player1Ward = maxWard;
+        Player1Health = maxHealth;
+        Player1MagicalPower = 0;
+        Player2Ward = maxWard;
+        Player2Health = maxHealth;
+        Player2MagicalPower = 0;
     }
 
+    /*
+    * UI components set by Unity
+    */
+
+    [SerializeField]
+    private Image[] player1Hearts = null;
+
+    [SerializeField]
+    private Sprite fullHeart = null;
+
+    [SerializeField]
+    private Sprite emptyHeart = null;
+
+    [SerializeField]
+    private int maxHealth = 0;
+
+    [SerializeField]
+    private int maxWard = 0;
 
     /*
     * Player 1 Info
@@ -29,77 +42,78 @@ public class UISingleton : Singleton<UISingleton>
 
     // private backing variables
 
-    private int _Player1Ward = -1;
-    private int _Player1Health = -1;
-    private int _Player1MagicalPower = -1;
+    private int _player1Ward;
+    private int _player1Health;
+    private int _player1MagicalPower;
 
     // public variables
 
     public int Player1Ward
     {
-        get { return _Player1Ward; }
+        get { return _player1Ward; }
         set
         {
-            if (_Player1Ward != value)
+            if (_player1Ward != value)
             {
-                _Player1WardChanged(_Player1Ward, value);
-                _Player1Ward = value;
+                Player1WardChanged(_player1Ward, value);
+                _player1Ward = value;
             }
         }
     }
 
     public int Player1Health
     {
-        get { return _Player1Health; }
+        get { return _player1Health; }
         set
         {
-            if (_Player1Health != value)
+            if (_player1Health != value)
             {
-                _Player1HealthChanged(_Player1Health, value);
-                _Player1Health = value;
+                Player1HealthChanged(_player1Health, value);
+                _player1Health = value;
             }
         }
     }
 
     public int Player1MagicalPower
     {
-        get { return _Player1MagicalPower; }
+        get { return _player1MagicalPower; }
         set
         {
-            if (_Player1MagicalPower != value)
+            if (_player1MagicalPower != value)
             {
-                _Player1MagicalPowerChanged(_Player1MagicalPower, value);
-                _Player1MagicalPower = value;
+                Player1MagicalPowerChanged(_player1MagicalPower, value);
+                _player1MagicalPower = value;
             }
         }
     }
 
     // change methods - update UI here
 
-    private void _Player1WardChanged(int oldValue, int newValue)
+    private void Player1WardChanged(int oldValue, int newValue)
     {
 
     }
 
-    private void _Player1HealthChanged(int oldValue, int newValue)
+    private void Player1HealthChanged(int oldValue, int newValue)
     {
-        for (int i = 0; i < Player1Hearts.Length; i++)
+        for (int i = 0; i < player1Hearts.Length; i++)
         {
             if (i < newValue)
             {
-                Player1Hearts[i].sprite = fullHeart;
+                player1Hearts[i].sprite = fullHeart;
             }
             else
             {
-                Player1Hearts[i].sprite = emptyHeart;
+                player1Hearts[i].sprite = emptyHeart;
             }
         }
     }
 
-    private void _Player1MagicalPowerChanged(int oldValue, int newValue)
+    private void Player1MagicalPowerChanged(int oldValue, int newValue)
     {
 
     }
+
 
     /*
     * Player 2 Info
@@ -107,64 +121,64 @@ public class UISingleton : Singleton<UISingleton>
 
     // private backing variables
 
-    private int _Player2Ward = -1;
-    private int _Player2Health = -1;
-    private int _Player2MagicalPower = -1;
+    private int _player2Ward;
+    private int _player2Health;
+    private int _player2MagicalPower;
 
     // public variables
 
     public int Player2Ward
     {
-        get { return _Player2Ward; }
+        get { return _player2Ward; }
         set
         {
-            if (_Player2Ward != value)
+            if (_player2Ward != value)
             {
-                _Player2WardChanged(_Player2Ward, value);
-                _Player2Ward = value;
+                Player2WardChanged(_player2Ward, value);
+                _player2Ward = value;
             }
         }
     }
 
     public int Player2Health
     {
-        get { return _Player2Health; }
+        get { return _player2Health; }
         set
         {
-            if (_Player2Health != value)
+            if (_player2Health != value)
             {
-                _Player2HealthChanged(_Player2Health, value);
-                _Player2Health = value;
+                Player2HealthChanged(_player2Health, value);
+                _player2Health = value;
             }
         }
     }
 
     public int Player2MagicalPower
     {
-        get { return _Player2MagicalPower; }
+        get { return _player2MagicalPower; }
         set
         {
-            if (_Player2MagicalPower != value)
+            if (_player2MagicalPower != value)
             {
-                _Player2MagicalPowerChanged(_Player2MagicalPower, value);
-                _Player2MagicalPower = value;
+                Player2MagicalPowerChanged(_player2MagicalPower, value);
+                _player2MagicalPower = value;
             }
         }
     }
 
     // change methods - update UI here
 
-    private void _Player2WardChanged(int oldValue, int newValue)
+    private void Player2WardChanged(int oldValue, int newValue)
     {
 
     }
 
-    private void _Player2HealthChanged(int oldValue, int newValue)
+    private void Player2HealthChanged(int oldValue, int newValue)
     {
 
     }
 
-    private void _Player2MagicalPowerChanged(int oldValue, int newValue)
+    private void Player2MagicalPowerChanged(int oldValue, int newValue)
     {
 
     }
