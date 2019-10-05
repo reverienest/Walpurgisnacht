517b173fab13f4220d84593a4fe334854b8d7c39
Albert Yiakiam Ko <Nimbro@lawn-128-61-63-210.lawn.gatech.edu>
2019-10-04 22:55:50 -0400

    Revert "Revert "Merge pull request #5 from TheGrim159/master""
    This reverts commit db806370fafe864e5071c522fb2ef57f437a57e7.

---
+++ Assets/Game/Singletons/InputMap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Action
+{
+    UP, DOWN, LEFT, RIGHT, PRIM, HEAVY, INTRIN, LAST_WORD, SPELL_CARD
+}
+
+public class InputMap : Singleton<InputMap>
+{
+    IDictionary<Action, KeyCode> act = new Dictionary<Action, KeyCode>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        act.Add(Action.UP, KeyCode.UpArrow);
+        act.Add(Action.DOWN, KeyCode.DownArrow);
+        act.Add(Action.LEFT, KeyCode.LeftArrow);
+        act.Add(Action.RIGHT, KeyCode.RightArrow);
+        act.Add(Action.PRIM, KeyCode.Z);
+        act.Add(Action.HEAVY, KeyCode.X);
+        act.Add(Action.INTRIN, KeyCode.C);
+        act.Add(Action.LAST_WORD, KeyCode.A);
+        act.Add(Action.SPELL_CARD, KeyCode.D);
+    }
+
+    bool GetInput(Action action)
+    {
+        return Input.GetKey(act[action]);
+    }
+    void UpdateKeyCode(Action action, KeyCode key)
+    {
+        act[action] = key;
+    }
+    bool GetInputUp(Action action)
+    {
+        return Input.GetKeyUp(act[action]);
+    }
+    bool GetInputDown(Action action)
+    {
+        return Input.GetKeyDown(act[action]);
+    }
+}
