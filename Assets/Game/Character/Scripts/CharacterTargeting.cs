ff580f189ced1ed195b113c878a3f71ba088fafa
Brandon Shockley <bshoc@bellsouth.net>
2019-10-05 14:16:36 -0400

    Added character targeting and orbiting reticle

---
+++ Assets/Game/Character/Scripts/CharacterTargeting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTargeting : MonoBehaviour
+{
+    public Transform target;
+
+    [SerializeField]
+    private GameObject reticlePrefab;
+
+    void Start()
+    {
+        //Create reticle
+        GameObject reticle = Instantiate(reticlePrefab, Vector3.zero, Quaternion.identity, transform);
+    }
+
+    public Vector2 TargetDirection() { return (target.position - transform.position).normalized; }
+}
