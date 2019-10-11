c4650e84e0092f357056d1af14f1bd3be0b4be3e
Arshad <iarsh930@gmail.com>
2019-10-11 00:51:55 -0400

    worked on circle charge

--- Assets/Build 0/Base Scripts/MagicCircle.cs
+++ Assets/Build 0/Base Scripts/MagicCircle.cs
@@ -1,21 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MagicCircle : MonoBehaviour
-{
-    public GameObject magicCircle;
-    public GameObject player;
+{    
+    /// <summary>
+     /// This is zero indexed
+     /// </summary>
+    public int playerNumber;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject MagicCircleSprite;
 
-    // Update is called once per frame
+    private float spawnTime = -1; 
+
+    private static readonly int startingMP = 25;
+    private static readonly int chargeTime = 10;
+    private static readonly float timeModifier = (100f - startingMP) / chargeTime;
+
     void Update()
     {
-        
+        if (spawnTime != -1) //make sure SpawnCircle called
+        {
+            int newGuageValue = Mathf.FloorToInt((Time.time - spawnTime) * timeModifier) + startingMP;
+            SetGuage(Mathf.Min(100, newGuageValue));
+        }
+    }
+
+    public void SpawnCircle(int playerNumber)
+    {
+        this.playerNumber = playerNumber;
+        spawnTime = Time.time;
+        SetGuage(startingMP);
+
+        //TODO: show circle sprite here
+    }
+
+    private void SetGuage(int percentage)
+    {
+        if (playerNumber == 0)
+        {
+            PlayerStatsManager.Instance.Player1MP = percentage;
+        }
+        else
+        {
+            PlayerStatsManager.Instance.Player2MP = percentage;
+        }
     }
 }
