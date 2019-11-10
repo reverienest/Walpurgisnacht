c590f2724a984dc1c6d22331e9911ec665100409
Arshad <iarsh930@gmail.com>
2019-11-10 01:06:40 -0500

    Ward Recharge

--- Assets/Game/Character/MagicCircle/Scripts/MagicCircle.cs
+++ Assets/Game/Character/MagicCircle/Scripts/MagicCircle.cs
@@ -15,24 +15,92 @@ public class MagicCircle : MonoBehaviour
     [SerializeField]
     private int startingMP = 0;
 
-    private float timeModifier;
-    private float lastUpdate = -1;
+    [SerializeField]
+    private float timePerWard = 0;
+
+    private float timeModifierMPCharge;
+    private float lastUpdateMPCharge = -1;
+
+    private float lastUpdateWardRefill = -1;
+    private float lastWardRefillValue = 0;
+
+    private int restitutionMP = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        timeModifier =  ((float)circleChargeTime) / (PlayerStatsManager.Instance.MaxMP - startingMP);
+        PlayerStatsManager.Instance.Player1Wards = 0;
+        timeModifierMPCharge = ((float)circleChargeTime) / (PlayerStatsManager.Instance.MaxMP - startingMP);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (lastUpdate != -1) // make sure SpawnCircle called
+        if (ShouldRefill)
         {
-            if (Time.time - lastUpdate >= timeModifier)
+            float addedRefill = (Time.time - lastUpdateWardRefill) / timePerWard;
+
+            if (0 == GetGuage())
             {
-                lastUpdate = Time.time;
+                addedRefill /= 2;
+            }
+
+            float newWardRefillValue = lastWardRefillValue + addedRefill;
+            SetWardRefill(newWardRefillValue);
+            
+            if (newWardRefillValue > 1)
+            {
+                //ward refilled
+                restitutionMP -= Mathf.FloorToInt(100 * timePerWard / circleChargeTime);
+                newWardRefillValue = 0;
+            }
+
+            if (Time.time - lastUpdateMPCharge >= timeModifierMPCharge)
+            {
+                lastUpdateMPCharge = Time.time;
+                DecrementGuage();
+                restitutionMP += 1;
+            }
+
+            lastUpdateWardRefill = Time.time;
+            lastWardRefillValue = newWardRefillValue;
+
+        }
+        else if (IsCircleSpawned)
+        {
+            if (Time.time - lastUpdateMPCharge >= timeModifierMPCharge)
+            {
+                lastUpdateMPCharge = Time.time;
                 IncrementGuage();
+            }
+        }
+        
+    }
+
+    private bool IsCircleSpawned
+    {
+        get => lastUpdateMPCharge != -1;
+    }
+
+    private bool IsPlayerInCircle
+    {
+        get => lastUpdateWardRefill != -1;
+    }
+
+    private bool ShouldRefill
+    {
+        get
+        {
+            if (!IsPlayerInCircle) { return false; }
+
+
+            if (playerNumber == 0)
+            {
+                return PlayerStatsManager.Instance.Player1Wards < PlayerStatsManager.Instance.MaxHealth;
+            }
+            else
+            {
+                return PlayerStatsManager.Instance.Player2Wards < PlayerStatsManager.Instance.MaxHealth;
             }
         }
     }
@@ -40,8 +108,33 @@ public class MagicCircle : MonoBehaviour
     public void SpawnCircle(int playerNumber)
     {
         this.playerNumber = playerNumber;
-        lastUpdate = Time.time;
+        lastUpdateMPCharge = Time.time;
         SetGuage(startingMP);
+    }
+
+
+    private void SetWardRefill(float refillValue)
+    {
+        if (playerNumber == 0)
+        {
+            PlayerStatsManager.Instance.Player1WardRefill = refillValue;
+        }
+        else
+        {
+            PlayerStatsManager.Instance.Player2WardRefill = refillValue;
+        }
+    }
+
+    private int GetGuage()
+    {
+        if (playerNumber == 0)
+        {
+            return PlayerStatsManager.Instance.Player1MP;
+        }
+        else
+        {
+            return PlayerStatsManager.Instance.Player2MP;
+        }
     }
 
     private void SetGuage(int percentage)
@@ -66,5 +159,60 @@ public class MagicCircle : MonoBehaviour
         {
             PlayerStatsManager.Instance.Player2MP += 1;
         }
+    }
+
+    private void DecrementGuage()
+    {
+        if (playerNumber == 0)
+        {
+            PlayerStatsManager.Instance.Player1MP -= 1;
+        }
+        else
+        {
+            PlayerStatsManager.Instance.Player2MP -= 1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsPlayerInCircle)
+        {
+            //player has not left circle since entering
+            return;
+        }
+
+        if (playerNumber != collision.gameObject.GetComponentInParent<SharedCharacterController>().playerNumber)
+        {
+            //not the player who cast this circle
+            return;
+        }
+
+        if (playerNumber == 0)
+        {
+            restitutionMP = PlayerStatsManager.Instance.Player1MP;
+        }
+        else
+        {
+            restitutionMP = PlayerStatsManager.Instance.Player2MP;
+        }
+
+        lastUpdateWardRefill = Time.time;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (playerNumber != collision.gameObject.GetComponentInParent<SharedCharacterController>().playerNumber)
+        {
+            //not the same player
+            return;
+        }
+        
+        SetGuage(restitutionMP);
+
+        lastWardRefillValue = 0;
+        lastUpdateWardRefill = -1;
+
+        SetWardRefill(0);
     }
 }
