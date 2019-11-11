09985ad97085a425be712ae7d981899de002cf78
Brandon Shockley <bshoc@bellsouth.net>
2019-11-10 22:36:34 -0500

    Added quick reset and fixed animation pixel

---
+++ Assets/Game/Shared/QuickReset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuickReset : MonoBehaviour
+{
+    [SerializeField]
+    private string titleScene = null;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.O) && Input.GetKeyDown(KeyCode.M))
+            SceneManager.LoadScene(titleScene);
+    }
+}
